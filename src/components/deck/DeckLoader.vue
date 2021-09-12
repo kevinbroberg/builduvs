6eaeca0722752ab9b367a1530ee372d6aaca3558
kevinbroberg <kevin.broberg@gmail.com>
2021-09-12 18:28:17 -0400

    Rearrange, better names

--- src/components/deck/DeckLoader.vue
+++ src/components/deck/DeckLoader.vue
@@ -1,13 +1,33 @@
+<script setup>
+  function awful_name2version(name) {
+      let dot = '·'
+      if (name.endsWith(dot * 3))
+          return name.replace(dot * 3, "3")
+      if (name.endsWith(dot * 2))
+          return name.replace(dot * 2, "2")
+      if (name.endsWith(':::'))
+          return name.replace(dot, "6")
+      if (name.endsWith('::'))
+          return name.replace("::", "4")
+      if (name.endsWith('::' + dot))
+          return name.replace("::" + dot, "5")
+      if (name.endsWith(':' + dot))
+          return name.replace(":" + dot, "3")
+      if (name.endsWith(dot))
+          return name.replace(dot, "1")
+      return name
+    }
+</script>
+
 <template>
   <div id="deckLoader">
-    <q-btn @click="loadDeck" v-if="showInputComponents" label="Finalize deck">
+    <q-btn @click="replaceVuexDeck" v-if="showInputComponents" label="Finalize deck">
       <q-tooltip>Loads the deck from text into this app</q-tooltip>
     </q-btn>
     <q-file filled v-model="fileModel" @change="deck2Text($event)" label="Upload deck">
       <q-tooltip>Loads a text or TTS.json deck into the text area below</q-tooltip>
     </q-file>
-    <q-input outlined autogrow type="textarea" v-model="text" placeholder="Enter deck as text"
-      @fileLoaded="showTextLoadDeck($event)" />
+    <q-input outlined autogrow type="textarea" v-model="text" placeholder="Enter deck as text" />
     
   </div>
 </template>
@@ -26,10 +46,20 @@ export default {
   computed: {
     showInputComponents() { return !!this.text }
   },
-  methods: {
-    showTextLoadDeck(ev) {
-      this.text = ev
-      this.loadDeck()
+  methods: {  
+
+    deck2Text(event) {
+      const file = event.target.files[0];
+      const reader = new FileReader();
+      console.log(file)
+
+      if (file.type == 'application/json') {
+        // attempt to read as a tabletop simulator deck
+        reader.onload = e => this.text = this.tts2Text(e.target.result);
+      } else {
+        reader.onload = e => this.text = e.target.result;
+      }      
+      reader.readAsText(file);
     },
     tts2Text(input) {
       try {
@@ -52,20 +82,7 @@ export default {
         return ''
       }
     },
-    deck2Text(event) {
-      const file = event.target.files[0];
-      const reader = new FileReader();
-      console.log(file)
-
-      if (file.type == 'application/json') {
-        // attempt to read as a tabletop simulator deck
-        reader.onload = e => this.text = this.tts2Text(e.target.result);
-      } else {
-        reader.onload = e => this.text = e.target.result;
-      }      
-      reader.readAsText(file);
-    },
-    loadDeck() {
+    replaceVuexDeck() {
       // clear the previous contents
       this.$store.commit('deck/nuke')
 
@@ -80,22 +97,22 @@ export default {
       for (let idx in main) {
         let match = main[idx]
         let qty = Number.parseInt(match[1])
+        let myName = this.awful_name2version(match[2])
         // O(N*C) where C is # cards in the 'database', N is # of cards in your deck. I assume this will be awful performance but NO PREMATURE OPTIMIZATION
         // later-me finds: it's not even fucking slow. damn it all
         let actualCard = null
         for (let key in cards) { 
-          if (cards[key].name.toLowerCase() == match[2].toLowerCase()) { // TODO better case insensitive
+          if (cards[key].name.toLowerCase() == myName.toLowerCase()) { // TODO better case insensitive
             actualCard = cards[key]
             break;
           }
         }
-        // console.log(`Actual card for ${match[2]}: ${actualCard}`)
         if (actualCard) {
           for(let i = 0 ; i < qty; ++i) {
             this.$store.commit('deck/increment', actualCard)
           }
         } else {
-          console.log(`Missed ${key}`)
+          console.log(`Missed ${myName}`)
         }
       }
 
