bb7e19d1df105ee5cbf508dec537b668fe320284
kevinbroberg <kevin.broberg@gmail.com>
2021-09-13 18:50:24 -0400

    Display the main character at the top, and partition by resources

--- src/components/deck/DeckView.vue
+++ src/components/deck/DeckView.vue
@@ -2,22 +2,57 @@
   import { ref, computed } from 'vue'
   import { useStore } from 'vuex';
   const store = useStore()
-  const deck = computed(() => store.getters['deck/getDeck'] )
+  const deck = computed(() => store.getters['deck/getDeckList'] )
+  const mainChar = computed(() => store.getters['deck/getMain'])
   const simple = "Simple", type = "Types", symbol = "Symbols"
   const partitionOptions = [simple, type, symbol]
   const howPartition = ref(type)
 
+  function combinations(list) {
+    if (list.length == 1) {
+      return [list, []]
+    }
+    let result = [[...list]]
+    
+    function nless1(_, idx, original) {
+      return [...original.slice(0, idx), ...original.slice(idx + 1)]
+    }
+    result.push(...list.map(nless1)) // length 2 sublists
+    list.forEach(e => result.push([e])) // length 1
+    result.push([[]]) // length 0
+    return result
+  }
+
+  import Elements from 'components/cards/detail/Elements.vue'
+
   function simplePartition() {
     return [{'key': 'all', 'label': 'Deck', 'cards': deck.value}]
   }
+  // TODO this is the kind of thing unit tests are good for
   function symbolPartition() {
-    throw 'Symbol partition not implemented'
+    // TODO use an Elements component for the label
+    let groups = combinations(mainChar.value.resources)
+    console.log(groups)
+
+    let parts = groups.map(g => {
+      return { key: g, label: g ? g : "No Symbols", cards: []}
+    } )
+    console.log(parts)
+    console.log(parts)
+    deck.value.forEach( card => {
+      // every resource in the partition must have a matching resource in at least one of the cards
+      let index = parts.findIndex(part => part.key.every( resource => card.resources.some( cr => cr == resource)))
+      // sentinel just in case - resource matching should always succeed on the empty list, though
+      let use = index < 0 ? groups.length - 1 : index
+      
+      parts[use].cards.push(card)
+    })
+    return parts
   }
   function typePartition() {
-    let myDeck = Object.keys(deck.value).map(k => deck.value[k]) // nondeterministic order is less than perfect...
-    let types = new Set([...myDeck.map(card => card.type)])
+    let types = new Set([...deck.value.map(card => card.type)])
     return [...types].map(me => {
-      return {'key': me, 'label': me, 'cards': myDeck.filter(card => card.type === me)}
+      return {'key': me, 'label': me, 'cards': deck.value.filter(card => card.type === me)}
     })
   }
   
@@ -25,8 +60,9 @@
     switch(howPartition.value) {
         case type:
           return typePartition()
-        case simple:
         case symbol:
+          return symbolPartition()
+        case simple:
         default:
           return simplePartition()
     }
@@ -35,23 +71,32 @@
 
 <template>
   <q-select :options="partitionOptions" v-model="howPartition"/>
+
+  <q-item-section avatar v-if="mainChar">
+      <q-avatar>
+      <!-- TODO zoom into just the card art here -->
+      <img :src="require(`assets/images/card_images/${mainChar.asset}`)">
+      </q-avatar>  
+  </q-item-section>
+  <q-item-section>
+      <q-item-label lines="1">{{mainChar.name}} <Elements :card=mainChar /></q-item-label>
+  </q-item-section>
+
   <q-list bordered>
     <div v-for="partition in partitions" :key="partition.key">
       <q-separator/>
       <q-item-label header>{{partition.label}}</q-item-label>
       <q-item v-for="card in partition.cards" :key="card.asset">
         <q-item-section avatar>
-          <q-avatar>
+            <q-avatar>
             <!-- TODO zoom into just the card art here -->
             <img :src="require(`assets/images/card_images/${card.asset}`)">
-          </q-avatar>  
+            </q-avatar>  
         </q-item-section>
         <q-item-section>
-          <q-item-label lines="1">{{card.qty}} {{card.name}}</q-item-label>
+            <q-item-label lines="1">{{card.qty}} {{card.name}}</q-item-label>
         </q-item-section>
       </q-item>
-        <!-- 
-        -->
     </div>
   </q-list>
 </template>
