b08119d66a5d7b9b90041b741fc3885fd938a740
kevinbroberg <kevin.broberg@gmail.com>
2021-09-29 16:45:38 -0400

    Filter name dropdown as you type

--- src/components/filter/NamePicker.vue
+++ src/components/filter/NamePicker.vue
@@ -1,9 +1,21 @@
 <script setup>
+  import { computed, ref } from 'vue'
   import * as provider from 'assets/card_provider.js'
   const selections = provider.selections
-  const options = provider.filteredCards.value.map(c => c.name)
+  const options = computed(() => provider.filteredCards.value
+    .map(c => c.name)
+    .filter(name => name.toLowerCase().indexOf(extraFilter.value) > -1)
+  )
+
+  let extraFilter = ref('')
+  function filterNames(val, update, abort) {
+    update(() => extraFilter.value = val.toLowerCase())
+  }
+  
 </script>
 <template>
-    <q-select v-model="selections.name" :options=options use-input standout dense clearable
-          new-value-mode="add" label="Search by name" class="q-ml-sm" />
+    <q-select v-model="selections.name" :options=options 
+      use-input standout dense clearable
+      new-value-mode="add" 
+      label="Search by name" class="q-ml-sm" @filter=filterNames />
 </template>
