6bb621140ae8c4a7a02bbb04fa04464e995c9a19
kevinbroberg <kevin.broberg@gmail.com>
2022-02-10 20:59:47 -0500

    Consolidate multiple copies of face

--- src/store/deck/getters.js
+++ src/store/deck/getters.js
@@ -19,13 +19,13 @@ export function getSideList(state) {
     return Object.keys(state.side).map(k => state.side[k])
 }
 export function getDeckText(state) {
-    let name = getFace(state)?.name // mainchar may be undefined
-    let face = name ? [{ name: name, qty: 1 }] : [] // if it's not, there is 1 copy in your deck
-    let deck = [...face, ...getDeckList(state)].map(c => `${c.qty} ${c.name}`)
+    let face = getFace(state) // mainchar may be undefined
+    let faceObj = face ? [{ name: face.name, qty: quantity(state)(face.asset) }] : []
+    let deck = [...faceObj, 
+        ...getDeckList(state).filter(c => c.asset != face.asset)
+    ].map(c => `${c.qty} ${c.name}`)
     
     let side = hasSide(state) ? ['sideboard', ...getSideList(state).map(c => `${c.qty} ${c.name}`)] : []
-    // maybe TODO sometime: accumulate additional copies of main char into the 1st quantity, vs 1 Amy... 3 Amy that will happen now
-    // a possible method: simply increment() face before exporting? or have a general dedupe method, and prepend face to the list before dedupe
     return [...deck, ...side ].join('\n')
 }
 export function getFace(state) {
