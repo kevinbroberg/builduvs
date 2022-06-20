d114ddb97ba094ad9ec75cfc05270ba9ac2f2805
kevin.broberg <kevin.broberg@gmail.com>
2022-06-20 14:55:14 -0400

    Crimson Rampage DLC

--- src/js/card_provider.js
+++ src/js/card_provider.js
@@ -1,10 +1,11 @@
 import { ref, computed } from 'vue'
+import rampage_dlc from 'assets/rampage_dlc.json'
 import rampage from 'assets/rampage.json'
 import provisional from 'assets/provs.json'
 import real_cards from 'assets/cards.json'
 // TODO sooner than later: pull out legacy / retro 
 
-export const cards = [...rampage, ...provisional, ...real_cards]
+export const cards = [...rampage_dlc, ...rampage, ...provisional, ...real_cards]
 export const symbolOptions = ["air", "all", "chaos", "death", "earth", "evil", "fire", "good", "infinity", "life", "order", "void", "water"]
 export const formatOptions = ["My Hero Academia","MHA banned", "standard","standard banned","retro","retro banned","unreleased","alpha", "alpha legend"]
 /*
