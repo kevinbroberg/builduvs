11321d6469c7c568e5ec65c9dd83a2952f9a3f57
kevin.broberg <kevin.broberg@gmail.com>
2022-06-21 22:37:31 -0400

    Move new promos to the head of the list

--- src/js/card_provider.js
+++ src/js/card_provider.js
@@ -5,7 +5,7 @@ import provisional from 'assets/provs.json'
 import real_cards from 'assets/cards.json'
 // TODO sooner than later: pull out legacy / retro 
 
-export const cards = [...rampage_dlc, ...rampage, ...provisional, ...real_cards]
+export const cards = [...rampage_dlc, ...provisional, ...rampage, ...real_cards]
 export const symbolOptions = ["air", "all", "chaos", "death", "earth", "evil", "fire", "good", "infinity", "life", "order", "void", "water"]
 export const formatOptions = ["My Hero Academia","MHA banned", "standard","standard banned","retro","retro banned","unreleased","alpha", "alpha legend"]
 /*
